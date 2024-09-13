40ab1b0e307ea04f72e09621611c10d4714ff297
iwpdev <glazoknet@gmail.com>
2024-09-13 19:18:19 +0200

    first commit

---
+++ src/php/Main.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Main theme class.
+ *
+ * @package iwpdev/theme
+ */
+
+namespace Iwpdev\Theme;
+
+/**
+ * Main class file.
+ */
+class Main {
+
+	/**
+	 * Theme version.
+	 */
+	const IWP_VERSION = '1.0.0';
+
+	/**
+	 * Main construct.
+	 */
+	public function __construct() {
+$this->init();
+	}
+
+	/**
+	 * Init Action and filters.
+	 *
+	 * @return void
+	 */
+	public function init():void {
+
+	}
+}
